d8a87e5b54031cc214f216c90257869780ae9d8c
Chinmay Deosthali <deosthali.chinmay6@gmail.com>
2021-08-19 17:35:31 -0400

    Added context

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,25 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
-
+import { useHistory } from "react-router-dom";
 function NewMeetupPage(){
+    const history = useHistory();
+    function addMetupHandler(meetupData){
+        fetch(
+            'https://react-getting-started-e2036-default-rtdb.firebaseio.com/meet-ups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json' 
+                }
+            }
+        ).then(() =>{
+            history.replace('/');
+        });
+    }
+
     return (<section>
         <h1>Add New Meetup</h1>
-        <NewMeetupForm />
+        <NewMeetupForm onAddMeetup={addMetupHandler}/>
     </section>);
 }
 
